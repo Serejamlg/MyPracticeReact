2fa7ae5a103744dd35be38d59fd1602f68d00d5b
Serejamlg <navisergo@mail.ru>
2022-07-17 11:35:20 +0300

    Поработал над Компанентами и импортом + экспорт

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Tehnologies from './Technologies';
+function App() {
+  return (
+    <div>
+      
+        <Header/>
+        <Tehnologies/>
+      </div>);
+}
+
+
+
+
+
+export default App;
+
---
+++ src/Technologies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Tehnologies = () => {
+    return (
+      <div>
+      <ul>
+        <li>Css</li>
+        <li>html</li>
+        <li>js </li>
+        <li>React</li>
+      </ul>
+    </div>
+     );
+  }
+
+  export default Tehnologies;
